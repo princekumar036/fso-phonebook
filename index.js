bffdd87c9a9859c18f9c634a4e7ec48d311e5632
Prince Kumar <princekumar036@gmail.com>
2024-01-21 20:35:45 +0100

    3.22

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
@@ -6,7 +7,7 @@ const Person = require('./models/person')
 
 // Create a new morgan token
 morgan.token('body', (req, res) => {
-    return JSON.stringify(req.body)
+	return JSON.stringify(req.body)
 })
 
 const app = express()
@@ -17,85 +18,84 @@ app.use(express.static('dist'))
 
 // HOMEPAGE
 app.get('/', (req, res) => {
-    res.send('Welcome to Persons API')
+	res.send('Welcome to Persons API')
 })
 
 // INFO PAGE
 app.get('/info', (req, res, next) => {
-    Person.countDocuments({}).then(count => {
-        const body = `Phonebook has info for ${count} people
+	Person.countDocuments({}).then(count => {
+		const body = `Phonebook has info for ${count} people
         <br/>
         ${String(new Date())}
         `
-        res.send(body)
-    })
-    .catch(err => next(err))
+		res.send(body)
+	})
+		.catch(err => next(err))
 })
 
 // GET ALL
 app.get('/api/persons', (req, res, next) => {
-    Person.find({})
-        .then(results => res.json(results))
-        .catch(err => next(err))
+	Person.find({})
+		.then(results => res.json(results))
+		.catch(err => next(err))
 })
 
 // GET ONE
 app.get('/api/persons/:id', (req, res, next) => {
-    Person.findById(req.params.id)
-        .then(result => {
-            if (result) {
-                res.json(result)
-            } else {
-                console.log(error)
-                res.status(404).end()
-            }
-        })
-        .catch(err => next(err))
+	Person.findById(req.params.id)
+		.then(result => {
+			if (result) {
+				res.json(result)
+			} else {
+				res.status(404).end()
+			}
+		})
+		.catch(err => next(err))
 })
 
 // ADD ONE
 app.post('/api/persons', (req, res, next) => {
-    const body = req.body
+	const body = req.body
 
-    const newPerson = new Person ({
-        name: body.name,
-        number: body.number
-    })
-    newPerson.save()
-        .then(savedPerson => {
-            res.json(savedPerson)
-        })
-        .catch(err => {next(err)})
+	const newPerson = new Person ({
+		name: body.name,
+		number: body.number
+	})
+	newPerson.save()
+		.then(savedPerson => {
+			res.json(savedPerson)
+		})
+		.catch(err => {next(err)})
 })
 
 // DELETE ONE
 app.delete('/api/persons/:id', (req, res, next) => {
-    Person.findByIdAndDelete(req.params.id)
-        .then(result => res.status(204).end())
-        .catch(err => next(err))
+	Person.findByIdAndDelete(req.params.id)
+		.then(result => res.status(204).end())
+		.catch(err => next(err))
 })
 
 // UPDATE ONE
 app.put('/api/persons/:id', (req, res, next) => {
-    const { name, number } = req.body
-    Person.findByIdAndUpdate(req.params.id, { name, number }, { new: true, runValidators: true })
-        .then(updatedPerson => {
-            res.json(updatedPerson)
-        })
-        .catch(err => next(err))
+	const { name, number } = req.body
+	Person.findByIdAndUpdate(req.params.id, { name, number }, { new: true, runValidators: true })
+		.then(updatedPerson => {
+			res.json(updatedPerson)
+		})
+		.catch(err => next(err))
 })
 
 // Error Handler Middlerware
 const errorHandler = (err, req, res, next) => {
-    console.log(err.message)
+	console.log(err.message)
 
-    if (err.name === 'CastError') {
-        return res.status(400).send({'error': 'malformatted id'})
-    } else if (err.name === 'ValidationError') {
-        return res.status(400).json({'error': err.message})
-    }
+	if (err.name === 'CastError') {
+		return res.status(400).send({ 'error': 'malformatted id' })
+	} else if (err.name === 'ValidationError') {
+		return res.status(400).json({ 'error': err.message })
+	}
 
-    next(err)
+	next(err)
 }
 app.use(errorHandler)
 
